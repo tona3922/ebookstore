bfd9392f10976afa6e3012b0d417febdded70a28
Congtran1452002 <79902179+Congtran1452002@users.noreply.github.com>
2022-11-11 18:47:17 +0700

    update login

--- src/authen/login.jsx
+++ src/authen/login.jsx
@@ -1,8 +1,36 @@
 import React from "react";
+import logo from "../img/logo.png";
+// import login_icon from "../img/login-icon.png"
+// import back_icon from "../img/back.png"
+import "./login.css"
+
 
 export const Login = () => {
     return(
-        <h1>Login</h1>
+        <div className="login-page">
+            <div className="login-container">
+                <header className="login-header">
+                    <img className="login-logo" src={logo} alt="" />
+                    <h1>ĐĂNG NHẬP</h1>
+                </header>
+                <div className="login-content">
+                    <label className="login-label" htmlFor="">Tên đăng nhập</label>
+                    <input className="login-input" type="text" placeholder="Tên đăng nhập"/>
+                    <label className="login-label" htmlFor="">Mật khẩu</label>
+                    <input className="login-input" type="text" placeholder="Mật khẩu"/>
+                    <button className="btn">
+                        {/* <img src={back_icon} alt="" /> */}
+                        Quay lại
+                    </button>
+                    <button className="btn">
+                        {/* <img src={login_icon} alt="" /> */}
+                        Đăng nhập
+                    </button>
+                </div>
+            </div>
+        </div>   
     );
 }
+
+
 
